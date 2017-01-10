6ebca45a00e3234e1de76131397acd85eb9a32ef
brandon220997 <brandon.cramer1997@gmail.com>
2017-01-10 20:32:23 +1100

    removed title bar and added white background to MainActivity changed 'test' text colour

--- app/src/main/java/com/class_creator/classcreator/MainActivity.java
+++ app/src/main/java/com/class_creator/classcreator/MainActivity.java
@@ -2,6 +2,7 @@ package com.class_creator.classcreator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
