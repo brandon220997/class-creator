b941013425caf9431e88fe65f041b562de3015a2
brandon220997 <brandon.cramer1997@gmail.com>
2017-01-17 14:13:05 +0700

    Minor change to android:id placement Renamed ClassCreator to createClass

--- app/src/main/java/com/class_creator/classcreator/MainActivity.java
+++ app/src/main/java/com/class_creator/classcreator/MainActivity.java
@@ -8,18 +8,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ClassCreatorButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ClassCreatorButton = (Button) findViewById(R.id.createclass);
+        Button createClassButton = (Button) findViewById(R.id.createclass);
 
-        ClassCreatorButton.setOnClickListener(new View.OnClickListener() {
+        createClassButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-
+                public void onClick(View view){
                 launchClassCreatorActivity();
             }
         });
